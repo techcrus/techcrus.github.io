6d773c26e9adaffec65ec2bd563c9bdd90c3d17a
Joel Thomson <joelparavara@gmail.com>
2019-05-08 17:22:51 +0530

    main

---
+++ main.js
@@ -0,0 +1,19 @@
+function calc() {
+  var a = parseInt(document.querySelector("#value1").value);
+  var b = parseInt(document.querySelector("#value2").value);
+  var op = document.querySelector("#operator").value;
+  var calculate;
+
+
+  if (a != nil && b != nil && op == "add") {
+    calculate = a + b;
+  } else if (op == "min") {
+    calculate = a - b;
+  } else if (op == "div") {
+    calculate = a / b;
+  } else if (op == "mul") {
+    calculate = a * b;
+  }
+
+  console.log(calculate);
+}
